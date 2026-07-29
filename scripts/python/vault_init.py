855893a00d2b0da0224d3b78be5664a7ea8572f3
h0ttab <myemail@example.com>
2026-07-29 21:55:49 +0300

    Fix vault init script

--- scripts/python/vault_init.py
+++ scripts/python/vault_init.py
@@ -10,12 +10,14 @@ args = arg_parser.parse_args()
 vault_ip = args.host
 vault_port = args.port
 vault_init_url = f"http://{vault_ip}:{vault_port}/v1/sys/init"
-vault_init_filepath = '../../secrets/vault_init.json'
+vault_init_filepath = './secrets/vault_init.json'
 
 vault_initialized = requests.get(vault_init_url).json()["initialized"]
 
 if vault_initialized:
     print(f"HashiCorp Vault on host {vault_ip} has already been initialized")
+else:
+    print(f"HashiCorp Vault is not initialized. Initializing now...")
 
 vault_unseal_data = requests.post(vault_init_url, json={
     "secret_shares": 1,
@@ -23,7 +25,7 @@ vault_unseal_data = requests.post(vault_init_url, json={
 }).json()
 
 with open(vault_init_filepath, 'w') as file:
-    if vault_unseal_data["errors"] != None:
+    if "errors" in vault_unseal_data:
         raise Exception(vault_unseal_data["errors"])
     file.write(json.dumps(vault_unseal_data))
     print(f"HashiCorp Vault on host {vault_ip} has been initialized. Bootstrap json has been saved to {vault_init_filepath}")
