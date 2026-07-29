f75ef26df4e2603731800bdf7c6ccb22a6a6da77
h0ttab <myemail@example.com>
2026-07-30 00:19:18 +0300

    Improve Vault init script

--- scripts/python/vault_init.py
+++ scripts/python/vault_init.py
@@ -39,45 +39,44 @@ def save_unseal_keys(keys_json):
             raise Exception(keys_json["errors"])
         
         file.write(json.dumps(keys_json))
-        print(f"HashiCorp Vault on host {VAULT_IP} has been initialized. Bootstrap json has been saved to {VAULT_INIT_FILE_PATH}")
+        print(f"Bootstrap keys have been saved to {VAULT_INIT_FILE_PATH}\n")
 
 def get_seal_status():
     response = requests.get(f"{VAULT_API_URL}/sys/seal-status")
     data = response.json()
-    if data["sealed"] is None:
+    if "sealed" not in data:
         raise Exception(f"Vault seal status is unknown. Response: \n{data}")
     else:
         return data["sealed"]
 
 def unseal_vault(key):
-    payload = {"key": key}
+    payload = json.dumps({"key": key})
     
-    response = requests.post(f"{VAULT_API_URL}/sys/init", json.dumps(payload))
+    response = requests.put(f"{VAULT_API_URL}/sys/unseal", payload)
     data = response.json()
 
-    if data["sealed"] is None or data["sealed"] is False:
-        raise Exception(f"Failed to unseal vault. Response: \n{data}")
-    else:
-        print("Vault has been successfuly unsealed")
+    if "errors" in data or "sealed" not in data:
+        raise Exception(f"Error while unsealing Vault: {data["errors"]}")
 
 
 if __name__ == "__main__":
 
     if get_initialization_status() is True:
-        print(f"HashiCorp Vault on host {VAULT_IP} has already been initialized. Trying to unseal...")    
+        print(f"HashiCorp Vault on host {VAULT_IP} has already been initialized. Trying to unseal...\n")    
     else:
         print(f"HashiCorp Vault is not initialized. Initializing now...\n")
         unseal_keys_json = get_unseal_keys(is_initialized=False)
         save_unseal_keys(unseal_keys_json)
-        print(f"Vault initialized!")
+        print(f"Vault initialized!\n")
 
     if get_seal_status() is True:
-        print("Fetching unseal keys...")
-        keys = get_unseal_keys(is_initialized=True)
-        print("Keys fetched! Unsealing Vault...")
-        for key in keys:
+        print("Fetching unseal keys...\n")
+        bootstrap_keys = get_unseal_keys(is_initialized=True)
+        print("Keys fetched! Unsealing Vault...\n")
+        print
+        for key in bootstrap_keys["keys"]:
             unseal_vault(key)
-        print(f"All keys used. Vault seal status is: {get_seal_status()}")
+        print(f"All keys used. Vault seal status is: {'SEALED' if get_seal_status() else 'UNSEALED'}")
     else:
-        print("Vault is already unsealed. Exiting...")
-        exit(1)
+        print("Vault is unsealed. Exiting...\n")
+        exit(0)
