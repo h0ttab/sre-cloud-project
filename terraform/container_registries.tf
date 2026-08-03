0a010e4c64f34e6a1755978ae43d84f426d78141
h0ttab <myemail@example.com>
2026-08-03 20:35:54 +0300

    Add YCR repositories and image lifecycle policies to Terraform IaC

--- terraform/container_registries.tf
+++ terraform/container_registries.tf
@@ -10,3 +10,31 @@ resource "yandex_container_registry" "container_registry" {
 output "container_registry_id" {
   value = "Yandex Container Registry ID: ${yandex_container_registry.container_registry.id}"
 }
+
+resource "yandex_container_repository" "container_repositories" {
+  for_each = var.repositories
+  name     = "${yandex_container_registry.container_registry.id}/${each.value}"
+}
+
+variable "repositories" {
+  type = set(string)
+  default = [
+    "shareit-server",
+    "shareit-gateway"
+  ]
+}
+
+resource "yandex_container_repository_lifecycle_policy" "container_repository_lifecycle_policy" {
+  for_each      = yandex_container_repository.container_repositories
+  name          = "lifecycle-policy-${each.key}"
+  repository_id = each.value.id
+  status        = "active"
+
+  rule {
+    description   = "Delete all untagged images and images older than 24h. Keep only one latest image."
+    untagged      = true
+    tag_regexp    = ".*"
+    retained_top  = 1
+    expire_period = "24h"
+  }
+}
