0f9b67f382ee4a1ba9440720e2626825ddf2f9b2
h0ttab <myemail@example.com>
2026-08-05 20:14:24 +0300

    Improve vault configuration script reliability

--- scripts/bash/vault_config_terraform.sh
+++ scripts/bash/vault_config_terraform.sh
@@ -1,6 +1,11 @@
 export VAULT_ADDR="http://$1:8200"
 export VAULT_TOKEN=$(cat ./secrets/vault/vault_root_token)
 
+if [[ -z "$VAULT_ADDR" || -z "$VAULT_TOKEN" ]]; then
+    echo "Vault address must be specified as a parameter and vault root token file must exist at ./secrets/vault/vault_root_token"
+    exit 1
+fi
+
 vault auth enable -path=terraform-approle approle
 
 vault policy write terraform-admin - <<EOF
